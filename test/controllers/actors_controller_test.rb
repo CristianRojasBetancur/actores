b4fd59ff916a2ca0a68f9adc48d9033bfca3ebc0
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-09-06 16:17:55 -0500

    Listando actores

--- test/controllers/actors_controller_test.rb
+++ test/controllers/actors_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ActorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get actors_index_url
+    assert_response :success
+  end
+
 end
